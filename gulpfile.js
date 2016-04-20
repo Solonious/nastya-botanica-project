bf6eea9f06e9d15a93f5ef0908f3d6be0d70e804
Solonious <sln07@bk.ru>
2016-04-20 16:48:20 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var opn = require('opn');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+
+
+
+
+//Launch server
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 8000
+  });
+  opn('http://localhost:8000', {app: 'firefox'});
+});
+
+//html
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+//css
+gulp.task('css', function () {
+  gulp.src('./app/css/*.css')
+    .pipe(connect.reload());
+});
+
+//js
+gulp.task('js', function () {
+  gulp.src('./app/js/*.js')
+    .pipe(connect.reload());
+});
+
+//watch
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/js/*.js'], ['js']);
+});
+
+//build
+gulp.task('build', function() {
+	gulp.src('./app/*.html')
+  	.pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(gulp.dest('dist'));
+});
+
+//default
+gulp.task('default', ['connect', 'watch']);
